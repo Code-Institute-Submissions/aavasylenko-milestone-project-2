cc5e85bb249bfe3fe9d8d11268b19ff917ef2dc9
aavasylenko <55499046+aavasylenko@users.noreply.github.com>
2020-06-29 15:45:19 -0500

    Added a function to save and pull completed task from the local storage
    Unable to directly push the changes from GitPod due to unknown error: 
    remote: Permission to aavasylenko/milestone-project-2.git denied to aavasylenko.
    fatal: unable to access 'https://github.com/aavasylenko/milestone-project-2.git/': The requested URL returned error: 403

--- app.js
+++ app.js
@@ -7,10 +7,11 @@ const background = document.querySelector('.navbar-nav');
 
 //Event Listeners
 document.addEventListener('DOMContentLoaded', setBackground);
-document.addEventListener('DOMContentLoaded', getTasks);
+document.addEventListener('DOMContentLoaded', getCompletedTasks);
+document.addEventListener('DOMContentLoaded', getUncompletedTasks);
 //Prevent empty input
 taskButton.addEventListener('click', function (event) {
-    const taskVal = taskInput.value.replace(/^\s+|\s+$/gm,'');
+    const taskVal = taskInput.value.replace(/^\s+|\s+$/gm, '');
     if (taskVal == "" || taskVal == null) {
         event.preventDefault();
         alert('Please type something...');
@@ -19,6 +20,8 @@ taskButton.addEventListener('click', function (event) {
     }
 });
 taskList.addEventListener('click', checkDelete);
+window.addEventListener('unload', saveCompletedtasks);
+window.addEventListener('unload', saveUncompletedTasks);
 filterOption.addEventListener('click', filterTasks);
 background.addEventListener('click', changeBg);
 
@@ -27,60 +30,58 @@ background.addEventListener('click', changeBg);
 //Choose your own background
 function changeBg(event) {
     const bgItem = event.target;
-    switch(bgItem.classList[1]) {
+    switch (bgItem.classList[1]) {
         case 'ocean':
-        document.body.style.background = "url('./ocean.jpg') no-repeat center";
-        document.body.style.backgroundSize = 'cover';
-        document.body.style.backgroundAttachment = 'fixed, scroll';
-        localStorage.setItem('background', JSON.stringify(bgItem.classList[1]));
-        break;
+            document.body.style.background = "url('./ocean.jpg') no-repeat center";
+            document.body.style.backgroundSize = 'cover';
+            document.body.style.backgroundAttachment = 'fixed, scroll';
+            localStorage.setItem('background', JSON.stringify(bgItem.classList[1]));
+            break;
         case 'mountain':
-        document.body.style.background = "url('./mountain.jpg') no-repeat center";
-        document.body.style.backgroundSize = 'cover';
-        document.body.style.backgroundAttachment = 'fixed, scroll';
-        localStorage.setItem('background', JSON.stringify(bgItem.classList[1]));
-        break;
+            document.body.style.background = "url('./mountain.jpg') no-repeat center";
+            document.body.style.backgroundSize = 'cover';
+            document.body.style.backgroundAttachment = 'fixed, scroll';
+            localStorage.setItem('background', JSON.stringify(bgItem.classList[1]));
+            break;
         case 'desert':
-        document.body.style.background = "url('./desert.jpg') no-repeat center";;
-        document.body.style.backgroundSize = 'cover';
-        document.body.style.backgroundAttachment = 'fixed, scroll';
-        localStorage.setItem('background', JSON.stringify(bgItem.classList[1]));
-        break;
+            document.body.style.background = "url('./desert.jpg') no-repeat center";;
+            document.body.style.backgroundSize = 'cover';
+            document.body.style.backgroundAttachment = 'fixed, scroll';
+            localStorage.setItem('background', JSON.stringify(bgItem.classList[1]));
+            break;
         case 'jungle':
-        document.body.style.background = "url('./jungle.jpg') no-repeat center";
-        document.body.style.backgroundSize = 'cover';
-        document.body.style.backgroundAttachment = 'fixed, scroll';
-        localStorage.setItem('background', JSON.stringify(bgItem.classList[1]));
-        break;
+            document.body.style.background = "url('./jungle.jpg') no-repeat center";
+            document.body.style.backgroundSize = 'cover';
+            document.body.style.backgroundAttachment = 'fixed, scroll';
+            localStorage.setItem('background', JSON.stringify(bgItem.classList[1]));
+            break;
     }
 }
 
 //Pulls last chosen background from the local storage
-function setBackground(){
+function setBackground() {
     const storedBg = JSON.parse(localStorage.getItem('background'));
     if (storedBg === 'ocean') {
         document.body.style.background = "url('./ocean.jpg') no-repeat center";
         document.body.style.backgroundSize = 'cover';
         document.body.style.backgroundAttachment = 'fixed, scroll';
-    } else if (storedBg === 'mountain'){
+    } else if (storedBg === 'mountain') {
         document.body.style.background = "url('./mountain.jpg') no-repeat center";
         document.body.style.backgroundSize = 'cover';
         document.body.style.backgroundAttachment = 'fixed, scroll';
-  } else if (storedBg === 'desert')
-    {
+    } else if (storedBg === 'desert') {
         document.body.style.background = "url('./desert.jpg') no-repeat center";
         document.body.style.backgroundSize = 'cover';
         document.body.style.backgroundAttachment = 'fixed, scroll';
-  } else if (storedBg === 'jungle')
-    {
+    } else if (storedBg === 'jungle') {
         document.body.style.background = "url('./jungle.jpg') no-repeat center";
         document.body.style.backgroundSize = 'cover';
         document.body.style.backgroundAttachment = 'fixed, scroll';
-  } else {
-      document.body.style.background = 'linear-gradient(180deg, rgb(116, 116, 116) , rgb(255, 255, 255))';
-      document.body.style.backgroundSize = 'cover';
-      document.body.style.backgroundAttachment = 'fixed, scroll';
-  }
+    } else {
+        document.body.style.background = 'linear-gradient(180deg, rgb(116, 116, 116) , rgb(255, 255, 255))';
+        document.body.style.backgroundSize = 'cover';
+        document.body.style.backgroundAttachment = 'fixed, scroll';
+    }
 }
 
 //Adds tasks
@@ -88,14 +89,12 @@ function addTask(event) {
     event.preventDefault();
     //Task Div
     const taskDiv = document.createElement('div');
-    taskDiv.classList.add('task');
+    taskDiv.classList.add('task', 'uncompleted');
     //Create li
     const newTask = document.createElement('li');
     newTask.innerText = taskInput.value;
     newTask.classList.add('task-item');
     taskDiv.appendChild(newTask);
-    //Add to local storage
-    saveLocalTasks(taskInput.value);
     //Checked button
     const completedButton = document.createElement('button');
     completedButton.innerHTML = '<i class ="fa fa-check"></fa>';
@@ -118,7 +117,7 @@ function checkDelete(e) {
     if (item.classList[0] === 'delete-btn') {
         const task = item.parentElement;
         task.classList.add('fall');
-        removeLocalTasks(task);
+        //removeLocalTasks(task);
         task.addEventListener('transitionend', function () {
             task.remove();
         })
@@ -128,9 +127,26 @@ function checkDelete(e) {
     if (item.classList[0] === 'complete-btn') {
         const task = item.parentElement;
         task.classList.toggle('completed');
+        task.classList.remove('uncompleted');
     }
 }
+//Save completed tasks to local storage
+function saveCompletedtasks(e) {
+    const comptask = document.getElementsByClassName('completed');
+    for (var i = 0; i < comptask.length; i++) {
+        var ctask = comptask[i].innerText;
+        let comptasks;
+        if (localStorage.getItem('comptasks') === null) {
+            comptasks = [];
+        } else {
+            comptasks = JSON.parse(localStorage.getItem('comptasks'));
+        }
+        comptasks.push(ctask);
+        localStorage.setItem('comptasks', JSON.stringify(comptasks))
+    };
+}
 
+//Filter tasks
 function filterTasks(e) {
     const tasks = taskList.childNodes;
     tasks.forEach(function (task) {
@@ -156,35 +172,22 @@ function filterTasks(e) {
     });
 }
 
-//Save to local storage
-function saveLocalTasks(task) {
-    let tasks;
-    if (localStorage.getItem('tasks') === null) {
-        tasks = [];
+//Pull completed tasks from local storage
+function getCompletedTasks() {
+    let comptasks;
+
+    if (localStorage.getItem('comptasks') === null) {
+        comptasks = [];
     } else {
-        tasks = JSON.parse(localStorage.getItem('tasks'));
+        comptasks = JSON.parse(localStorage.getItem('comptasks'));
     }
-    tasks.push(task);
-    localStorage.setItem('tasks', JSON.stringify(tasks))
-}
-
-
-//Pull from local storage
-function getTasks() {
-    let tasks;
-
-    if (localStorage.getItem('tasks') === null) {
-        tasks = [];
-    } else {
-        tasks = JSON.parse(localStorage.getItem('tasks'));
-    }
-    tasks.forEach(function (task) {
+    comptasks.forEach(function (comptask) {
         //Task Div
         const taskDiv = document.createElement('div');
-        taskDiv.classList.add('task');
+        taskDiv.classList.add('task', 'completed');
         //Create li
         const newTask = document.createElement('li');
-        newTask.innerText = task;
+        newTask.innerText = comptask;
         newTask.classList.add('task-item');
         taskDiv.appendChild(newTask);
         //Checked button
@@ -200,17 +203,55 @@ function getTasks() {
         //Append to list
         taskList.appendChild(taskDiv);
     });
+    localStorage.removeItem('comptasks');
 }
 
-//Delete from local storage
-function removeLocalTasks(task) {
-    let tasks;
-    if (localStorage.getItem('tasks') === null) {
-        tasks = [];
-    } else {
-        tasks = JSON.parse(localStorage.getItem('tasks'));
-    }
-    const taskIndex = task.children[0].innerText;
-    tasks.splice(tasks.indexOf(taskIndex), 1);
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+//Save to uncompleted tasks to local storage
+function saveUncompletedTasks(untask) {
+    const task = document.getElementsByClassName('uncompleted');
+    for (var i = 0; i < task.length; i++) {
+        var untask = task[i].innerText;
+        let tasks;
+        if (localStorage.getItem('tasks') === null) {
+            tasks = [];
+        } else {
+            tasks = JSON.parse(localStorage.getItem('tasks'));
+        }
+        tasks.push(untask);
+        localStorage.setItem('tasks', JSON.stringify(tasks))
+    };
 }
+
+    //Pull uncompleted tasks from local storage
+    function getUncompletedTasks() {
+        let tasks;
+
+        if (localStorage.getItem('tasks') === null) {
+            tasks = [];
+        } else {
+            tasks = JSON.parse(localStorage.getItem('tasks'));
+        }
+        tasks.forEach(function (task) {
+            //Task Div
+            const taskDiv = document.createElement('div');
+            taskDiv.classList.add('task', 'uncompleted');
+            //Create li
+            const newTask = document.createElement('li');
+            newTask.innerText = task;
+            newTask.classList.add('task-item');
+            taskDiv.appendChild(newTask);
+            //Checked button
+            const completedButton = document.createElement('button');
+            completedButton.innerHTML = '<i class ="fa fa-check"></fa>';
+            completedButton.classList.add('complete-btn');
+            taskDiv.appendChild(completedButton);
+            //Delete button
+            const deleteButton = document.createElement('button');
+            deleteButton.innerHTML = '<i class ="fa fa-trash"></fa>';
+            deleteButton.classList.add('delete-btn');
+            taskDiv.appendChild(deleteButton);
+            //Append to list
+            taskList.appendChild(taskDiv);
+        });
+        localStorage.removeItem('tasks');
+    };
